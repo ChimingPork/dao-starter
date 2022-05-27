f23850156e0169c579b3e70c9ffb80171d1b2fca
Josh Bellingham <92236549+ChimingPork@users.noreply.github.com>
2022-05-27 12:16:41 -0700

    Initial Commit with Metamask Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import { useAddress, useMetamask } from '@thirdweb-dev/react';
+
+
 const App = () => {
+  //Use hooks given by thirdweb
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+  console.log("👋 Address:", address);
+
+  // If no wallet is connected, add a button to call connectWallet
+  if (!address) {
   return (
     <div className="landing">
       <h1>Welcome to My DAO</h1>
+      <button onClick={connectWithMetamask} className="btn-hero">
+        Connect your wallet
+      </button>
     </div>
   );
 };
+
+//If wallet is connected, return this page instead
+return (
+  <div className="landing">
+    <h1>Wallet connected!</h1>
+  </div>
+)
+}
 
 export default App;
